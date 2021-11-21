7934f1e26db70b1c80cf9e5f4aab95317ce085f0
Louise <louise@zanier.org>
2021-11-21 17:08:57 +0100

    [frontend] Made the BookView prettier

--- frontend/src/components/BookView.vue
+++ frontend/src/components/BookView.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="book-view">
-    {{ book.title }}
+  <div class="book-view block card">
+    <header class="card-header">
+      <div class="card-header-title">
+        {{ book.title }}
+      </div>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        {{ book.description }}
+      </div>
+    </div>
+    <div class="card-footer">
+      <span v-for="(tag, i) in book.tags" :key="i" class="card-footer-item">{{ tag }}</span>
+    </div>
   </div>
 </template>
 
@@ -15,3 +27,9 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+.book-view {
+  width: 50vw;
+}
+</style>
